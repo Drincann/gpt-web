b8b80841d114a8e8f7daca3469c92464c80294ff
gaojunkang <gaojunkang@xiaohongshu.com>
2023-03-19 11:55:59 +0800

    feat: serve static

--- server/app.ts
+++ server/app.ts
@@ -5,7 +5,15 @@ import config from './config.js'
 import { setChatStorage, setCodeStorage, setIPSendCodeButNotVerifyPerDayStorage, setIPSendCodeExpireStorage } from './middlewares/storage'
 import type { AppState, AppContext } from './types'
 import { logger } from './utils/logger'
+import KoaStatic from 'koa-static'
+import path from 'node:path'
 
+// cmj patch
+import { fileURLToPath } from "node:url";
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+const staticPath = path.join(__dirname, 'public')
 const app = new Koa<AppState, AppContext>()
 app.use(KoaBody())
   .use(async (ctx, next) => {
@@ -20,7 +28,9 @@ app.use(KoaBody())
   .use(setIPSendCodeButNotVerifyPerDayStorage(1000 * 60 * 60 * 24))
   .use(rootRouter.routes())
   .use(rootRouter.allowedMethods())
+  .use(KoaStatic(staticPath))
   .listen(config.port, () => {
-    logger.info(`Server is running on port ${config.port}`)
+    logger.info(`[SERVER] Server is running on port ${config.port}`)
+    logger.info(`[SERVER] Static files are served from ${staticPath}`)
   })
 
