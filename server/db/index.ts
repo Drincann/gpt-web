633d046614a523718dcbfb0e9cc4189ae8f37a1a
Drincann <1019933576@qq.com>
2023-03-20 02:56:34 +0800

    fix: mysql connection reset

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,13 +2,20 @@ import mysql from 'mysql2/promise'
 import config from '../config.js'
 import { logger } from '../utils/logger.js'
 
-export const conn = await mysql.createConnection({
+export const conn = mysql.createPool({
   host: config.mysql.host,
   port: config.mysql.port,
   user: config.mysql.user,
   password: config.mysql.password,
   database: config.mysql.database,
-})
+  // pool
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0
+});
+
 /*
 CREATE TABLE `user`(
   `id` int NOT NULL AUTO_INCREMENT COMMENT '自增 id',
